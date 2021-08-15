59ea7678de8d3ee7edba3adb7118fe04708f1d42
Elena Kleimenova <elena-lewis@mail.ru>
2021-08-15 15:14:27 +0400

    refactoring pick and omit 2nd task

--- 02-javascript-data-types/2-pick/index.js
+++ 02-javascript-data-types/2-pick/index.js
@@ -6,19 +6,16 @@
  */
 export const pick = (obj, ...fields) => {
   const newObj = {};
-  const arrayObj = Object.entries(obj);
+  const arrayObj = Object.keys(obj);
 
-  for (let y = 0; y < fields.length; y++) {
-    for (let i=0; i < arrayObj.length; i++){
-
-      if(fields[y] == arrayObj[i][0]) {
-        newObj[fields[y]] = arrayObj[i][1];
-      }
-
-      else continue;
+  for (let value of arrayObj) {
+    if (fields.includes(value) === true){
+      newObj[value] = obj[value];
     }
 
+    continue;
   }
+
   return newObj;
 
 };
--- 02-javascript-data-types/3-omit/index.js
+++ 02-javascript-data-types/3-omit/index.js
@@ -5,20 +5,17 @@
  * @returns {object} - returns the new object
  */
 export const omit = (obj, ...fields) => {
-  const newObj = obj;
-  const arrayObj = Object.entries(obj);
+  const newObj = {};
+  const arrayObj = Object.keys(obj);
 
-  for (let y = 0; y < fields.length; y++) {
-    for (let i=0; i < arrayObj.length; i++){
-
-      if(fields[y] == arrayObj[i][0]) {
-        delete newObj[fields[y]];
-      }
-
-      else continue;
+  for (let value of arrayObj) {
+    if (!fields.includes(value) === true){
+      newObj[value] = obj[value];
     }
 
+    continue;
   }
+
   return newObj;
 
 };
